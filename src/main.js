c7dd3774385eba647cf6618761b90eb9c4d22472
Nathanael <natdutheuil@gmail.com>
2026-01-15 22:18:22 +0100

    fix index

--- src/main.js
+++ src/main.js
@@ -51,7 +51,7 @@ const mouse = new THREE.Vector2();
 
 // --- CHARGEMENT DE L'ORDINATEUR ---
 const loader = new GLTFLoader();
-loader.load('/models/computer.glb', (gltf) => {
+loader.load('./models/computer.glb', (gltf) => {
     console.log("Modèle chargé !"); 
     computer = gltf.scene;
     computer.scale.set(1, 1, 1);
